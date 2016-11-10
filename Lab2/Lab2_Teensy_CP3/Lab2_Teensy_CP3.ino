804f337c3c96af6cdf849bf8240364f8096b4c7f
JayKun <jackong95@hotmail.com>
2016-11-09 21:20:11 -0800

    Added CP3 Arduino and Teensy

--- Lab2/Lab2_Teensy_CP3/Lab2_Teensy_CP3.ino
+++ Lab2/Lab2_Teensy_CP3/Lab2_Teensy_CP3.ino
@@ -1,10 +1,17 @@
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
+#include "SPI.h" 
 
   uint64_t pipes[2] ={0xc2c2c2c2c2, 0xe7e7e7e7e7}; 
   RF24 radio(9, 10);
-  
+
+  struct Data
+{
+  uint8_t c[20]; 
+  uint16_t len = 0; 
+};
+
 void setup() {
   // put your setup code here, to run once: 
   radio.begin(); 
@@ -12,20 +19,21 @@ void setup() {
   radio.setChannel(23);
   radio.setPALevel(RF24_PA_MIN);
   printf_begin(); 
-  radio.openWritingPipe(pipes[0]); 
+  //radio.openWritingPipe(pipes[]); 
   radio.openReadingPipe(1, pipes[1]);
+
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    char c = 'k'; 
-    radio.write((char*)c, 1);
+    radio.startListening(); 
     if(radio.available())
     {
-      radio.startListening(); 
+     while(radio.available()) 
+      {
       char data ;
-      radio.read(&data, 1);
-      radio.stopListening(); 
+      radio.read((char*)&data, 1);
       Serial.println(data);
+      }
      }
+     
 }
