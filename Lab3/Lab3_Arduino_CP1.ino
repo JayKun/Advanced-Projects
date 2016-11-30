2cdb5deabad106243e92075d1a80c6b617e5d967
melonist <melonist@ucla.edu>
2016-11-29 19:43:22 -0800

    Final CP1

--- Lab3/Lab3_Arduino_CP1.ino
+++ Lab3/Lab3_Arduino_CP1.ino
@@ -1,5 +1,6 @@
 
-int sensorPin = A1;
+int sensorPin = A1; //Arduino pin A1
+
 void setup() {
   Serial.begin(9600); 
   pinMode(sensorPin, INPUT);
@@ -9,7 +10,8 @@ void loop() {
 
 int pinReading = analogRead(sensorPin); 
 delay(100);
-float voltageReading = pinReading*(3.3/1023); 
-Serial.println(voltageReading); 
+float voltageReading = pinReading*(3.338/1023); //Vout
+float finalVolt = voltageReading*8400/6600; //Vin
+Serial.println(finalVolt); 
 
 }
