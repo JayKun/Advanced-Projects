69b824499c978a31aa0ad57d5d122c82dfe9c00c
JayKun <jackong95@hotmail.com>
2016-12-01 14:37:42 -0800

    Added CP 4

--- Lab3/Lab3_Arduino_CP3_FINAL.ino
+++ Lab3/Lab3_Arduino_CP3_FINAL.ino
@@ -4,13 +4,16 @@
 #include "RF24.h"
 #include "RF24_config.h"
 
-#define buttonL 12; 
-#define buttonR 13; 
+#define buttonL 2 
+#define buttonR A3
+#define sensorPin A0
 
 
 struct Data {
-  char buttonLeft; 
-  char buttonRight;
+  uint8_t buttonLeft; 
+  uint8_t buttonRight;
+  uint8_t batteryLevel; 
+  //uint32_t batteryReading; 
 };
 
 Data packet; 
@@ -18,24 +21,31 @@ Data packet;
 uint64_t pipes[2] ={0xc2c2c2c2c2, 0xe7e7e7e7e7}; 
 RF24 radio(9, 10);
 
-void init(){
+void initRadio(){
+  radio.begin();
   radio.setChannel(23);
   radio.setPALevel(RF24_PA_MIN);
+  radio.setCRCLength(RF24_CRC_16);
+  radio.setPayloadSize(3);
   radio.openWritingPipe(pipes[1]); 
   radio.openReadingPipe(1, pipes[0]);
-  radio.setCRCLength(RF24_CRC_16);
-  radio.setPayloadSize(2);
-  radio.begin();
 }
 
 
 void setup() {
-  init(); 
+  Serial.begin(9600); 
+  initRadio();
+  printf_begin(); 
+  delay(1000); 
+  radio.printDetails(); 
 }
 
 void loop() {
     int leftReading = digitalRead(buttonL); 
     int rightReading = digitalRead(buttonR);
+    int batteryR = analogRead(sensorPin); 
+    float voltageReading = batteryR*(3.337/1023); 
+    voltageReading = (voltageReading/3.337)*4.2;
 
       if(leftReading == HIGH){
            packet.buttonLeft = 'H'; 
@@ -43,6 +53,15 @@ void loop() {
       if(rightReading == HIGH){
            packet.buttonRight = 'H'; 
         }
+        
+      if(voltageReading >= 3.9 )
+      packet.batteryLevel = 'G';
+
+      else if (voltageReading >= 3.7)
+      packet.batteryLevel ='Y';
+
+      else if(voltageReading < 3.7)
+      packet.batteryLevel = 'R';
     
     radio.write((Data*)&packet, sizeof(packet));
     
